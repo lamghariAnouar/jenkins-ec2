9976a4a7933dc7bbe1fbaf2654b29ec5f0987347
Anouar Lamghari <anouar.lamghari@berexia.com>
2024-02-29 12:31:15 +0100

    second commit

---
+++ src/main/java/com/autodeploy/jenkinsec2/JenkinsEc2Application.java
@@ -0,0 +1,13 @@
+package com.autodeploy.jenkinsec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsEc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/autodeploy/jenkinsec2/JenkinsEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autodeploy.jenkinsec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
